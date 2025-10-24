8e764fff0b5d9f83f62143cda7d274332fffd6e3
hillyleopard133 <brokfairy@gmail.com>
2025-10-24 12:10:33 +0100

    Greetings lab complete

--- app/src/main/java/ie/setu/mobileappdevassignment/MainActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevassignment/MainActivity.kt
@@ -1,20 +1,24 @@
 package ie.setu.mobileappdevassignment
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ie.setu.mobileappdevassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.greetingButton.setOnClickListener {
+            val greetingText = getString(R.string.greeting_text)
+            Toast.makeText(applicationContext, greetingText, Toast.LENGTH_LONG).show()
         }
     }
+
 }
