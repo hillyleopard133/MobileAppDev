a40e502aefd951a1625a5fea50e108451dc805fa
hillyleopard133 <brokfairy@gmail.com>
2025-10-25 19:16:05 +0100

    Ingredients can be deleted from recipe

--- app/src/main/java/ie/setu/mobileappdevassignment/adapters/IngredientAdapter.kt
+++ app/src/main/java/ie/setu/mobileappdevassignment/adapters/IngredientAdapter.kt
@@ -6,33 +6,38 @@ import androidx.recyclerview.widget.RecyclerView
 import ie.setu.mobileappdevassignment.databinding.CardIngredientBinding
 import ie.setu.mobileappdevassignment.models.IngredientModel
 
-class IngredientAdapter constructor(private var ingredients: List<IngredientModel>) :
+class IngredientAdapter constructor(private var ingredients: ArrayList<IngredientModel>) :
     RecyclerView.Adapter<IngredientAdapter.MainHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
         val binding = CardIngredientBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
 
-        return MainHolder(binding)
+        return MainHolder(binding, this)
     }
 
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val ingredient = ingredients[holder.adapterPosition]
-        holder.bind(ingredient)
+        val ingredient = ingredients[position]
+        holder.bind(ingredient, ingredients)
     }
 
     override fun getItemCount(): Int = ingredients.size
 
-    class MainHolder(private val binding : CardIngredientBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class MainHolder(private val binding : CardIngredientBinding, private val adapter: IngredientAdapter) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(ingredient: IngredientModel) {
+        fun bind(ingredient: IngredientModel, ingredients: ArrayList<IngredientModel>) {
             binding.ingredientName.text = ingredient.name
             binding.ingredientAmount.text = ingredient.amount.toString()
             if(ingredient.unit == "none"){
                 binding.ingredientUnit.text = ""
             }else{
                 binding.ingredientUnit.text = ingredient.unit
+            }
+
+            binding.btnDeleteIngredient.setOnClickListener {
+                ingredients.removeAt(position)
+                adapter.notifyItemRemoved(position)
+                adapter.notifyItemRangeChanged(position, ingredients.size)
             }
         }
     }
