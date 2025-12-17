fefe4ea92683836de59611e1c5842ea1a25dd369
hillyleopard133 <brokfairy@gmail.com>
2025-12-17 20:17:36 +0000

    Map markers

--- app/src/main/java/ie/setu/mobileappdevassignment/activities/RecipeMapsActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevassignment/activities/RecipeMapsActivity.kt
@@ -2,27 +2,49 @@ package ie.setu.mobileappdevassignment.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import ie.setu.mobileappdevassignment.databinding.ActivityRecipeMapsBinding
 import ie.setu.mobileappdevassignment.databinding.ContentRecipeMapsBinding
+import ie.setu.mobileappdevassignment.main.MainApp
 
 class RecipeMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecipeMapsBinding
     private lateinit var contentBinding: ContentRecipeMapsBinding
     lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        app = application as MainApp
+
         binding = ActivityRecipeMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         contentBinding = ContentRecipeMapsBinding.bind(binding.root)
         contentBinding.mapView.onCreate(savedInstanceState)
+
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
     }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.recipes.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
